ef8cb9b8c6ab331e08aad2765723668ff90d1c6e
sohailsayed <s7sayed@uwaterloo.ca>
2024-08-28 11:59:35 -0400

    create DB if it does not exist

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use futures::executor::block_on;
-use sea_orm::{Database, DbErr};
+use sea_orm::{ConnectionTrait, Database, DbBackend, DbErr, Statement};
 
 // hard-coding password for now, would not do this in production!
 const DATABASE_URL: &str = "postgres://postgres:password123@db:5432";
@@ -7,6 +7,26 @@ const DB_NAME: &str = "warehouse_db";
 
 async fn run() -> Result<(), DbErr> {
     let db = Database::connect(DATABASE_URL).await?;
+
+    let db = &match db.get_database_backend() {
+        DbBackend::MySql => db,
+        DbBackend::Postgres => {
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("DROP DATABASE IF EXISTS \"{}\";", DB_NAME),
+            ))
+            .await?;
+            db.execute(Statement::from_string(
+                db.get_database_backend(),
+                format!("CREATE DATABASE \"{}\";", DB_NAME),
+            ))
+            .await?;
+ 
+            let url = format!("{}/{}", DATABASE_URL, DB_NAME);
+            Database::connect(&url).await?
+        }
+        DbBackend::Sqlite => db,
+    };
 
     Ok(())
 }
