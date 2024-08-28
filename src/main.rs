db0ac8996c4f3f3bffb36f8fa61645fe8163d68f
sohailsayed <s7sayed@uwaterloo.ca>
2024-08-28 11:15:01 -0400

    simplify Dockerfile and compose file, connect to database server with SeaORM

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use futures::executor::block_on;
+use sea_orm::{Database, DbErr};
+
+// hard-coding password for now, would not do this in production!
+const DATABASE_URL: &str = "postgres://postgres:password123@db:5432";
+const DB_NAME: &str = "warehouse_db";
+
+async fn run() -> Result<(), DbErr> {
+    let db = Database::connect(DATABASE_URL).await?;
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting");
+    if let Err(err) = block_on(run()) {
+        panic!("{}", err);
+    }
+    else {
+        println!("Connected!");
+    }
 }
