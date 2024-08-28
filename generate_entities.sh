00bd47c800a343a860d6f450d4dbf6e3b64a1e97
sohailsayed <s7sayed@uwaterloo.ca>
2024-08-28 19:30:36 -0400

    fixup! generate entities with SeaORM, expose docker postgres service to local machine

--- generate_entities.sh
+++ generate_entities.sh
@@ -1,4 +1,4 @@
 # Much like the rest of this application, values, like the port and password here, are being hardcoded. This doesn't reflect how production code would look
 sea-orm-cli generate entity \
-    -u postgres://postgres:password123@db:54320/warehouse_db \
+    -u postgres://postgres:password123@localhost:54320/warehouse_db \
     -o src/entities
